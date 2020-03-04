3089d54c73d6b07e2aba49f7eb013f98b19f8cb3
alanaudi <alanaudi.tw@gmail.com>
2020-03-04 09:11:44 +0800

    (lib.utils): Convert xlsx to csv

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,22 @@
 """ Personal Utilities """
 
+import xlrd
+import csv
+
+
+
 class EasyDict(dict):
     def __init__(self, *args, **kwargs):
         super(EasyDict, self).__init__(self, *args, **kwargs)
         self.__dict__ = self
+
+
+def xlsx2csv(filename):
+    wb = xlrd.open_workbook(filename)
+    for name in wb.sheet_names():
+        sh = wb.sheet_by_name(name)
+        with open(F"{name}.csv", "w") as f:
+            wr = csv.writer(f)
+
+            for row in range(sh.nrows):
+                wr.writerow(sh.row_values(row))
