14f4c35ae1807142f6bf4e4502a6492f8b809a3b
alanaudi <alanaudi.tw@gmail.com>
2020-03-06 09:55:31 +0800

    (fix): Fix EasyDict initialization

--- lib/utils.py
+++ lib/utils.py
@@ -7,7 +7,7 @@ import csv
 
 class EasyDict(dict):
     def __init__(self, *args, **kwargs):
-        super(EasyDict, self).__init__(self, *args, **kwargs)
+        super(EasyDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
 
